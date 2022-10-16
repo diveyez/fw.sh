e7cd37b9e8a76ce04e2ff828f082a11d0ac014fe
Richard N <nemofbaltimore@gmail.com>
2022-10-16 03:16:02 -0400

    fixing ignores

--- .history/gameservers/fivem_20221016024043.sh
+++
@@ -1,4 +0,0 @@
-##### FiveM Server Rules
-sudo iptables -A INPUT -p udp --dport 30120 -j ACCEPT
-sudo iptables -A INPUT -p tcp --sport 30120 -j ACCEPT
-sudo iptables -A INPUT -p tcp --sport 40120 -j ACCEPT
